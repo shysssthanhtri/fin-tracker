e30b837fc5a972fd8934c74efac6cfb999ff4aeb
shysssthanhtri <shyssthanhtri@gmail.com>
2025-05-05 07:03:36 +0700

    feat: open create team dialog

--- src/app/_components/layout/team-switcher.tsx
+++ src/app/_components/layout/team-switcher.tsx
@@ -4,6 +4,15 @@ import { ChevronsUpDown, Plus } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
 import * as React from "react";
 
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -31,6 +40,9 @@ export function TeamSwitcher() {
   const activeTeamId = searchParams.get(Query.teamId);
   const activeTeam = teams?.find((team) => team.id === activeTeamId);
 
+  const [isCreateTeamDialogOpen, setIsCreateTeamDialogOpen] =
+    React.useState(false);
+
   const setActiveTeam = React.useCallback(
     (newActiveTeamId: TTeamEntity["id"]) => {
       router.push(Routes.input(newActiveTeamId));
@@ -40,52 +52,80 @@ export function TeamSwitcher() {
 
   if (!activeTeam && !isLoading) return null;
 
+  const dropdown = (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <SidebarMenuButton
+          size="lg"
+          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+          disabled={isLoading}
+        >
+          <div className="grid flex-1 text-left text-sm leading-tight">
+            <span className="truncate font-semibold">
+              {!!activeTeam && activeTeam.name}
+              {!!isLoading && "Loading"}
+            </span>
+          </div>
+          <ChevronsUpDown className="ml-auto" />
+        </SidebarMenuButton>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+        align="start"
+        side={isMobile ? "bottom" : "right"}
+        sideOffset={4}
+      >
+        <DropdownMenuLabel className="text-muted-foreground text-xs">
+          Teams
+        </DropdownMenuLabel>
+        {teams.map((team) => (
+          <DropdownMenuItem
+            key={team.name}
+            onClick={() => setActiveTeam(team.id)}
+            className="gap-2 p-2"
+          >
+            {team.name}
+          </DropdownMenuItem>
+        ))}
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          className="gap-2 p-2"
+          onClick={() => setIsCreateTeamDialogOpen(true)}
+        >
+          <div className="bg-background flex size-6 items-center justify-center rounded-md border">
+            <Plus className="size-4" />
+          </div>
+          <div className="text-muted-foreground font-medium">Add team</div>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+
+  const dialogContent = (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Are you absolutely sure?</DialogTitle>
+        <DialogDescription>
+          This action cannot be undone. Are you sure you want to permanently
+          delete this file from our servers?
+        </DialogDescription>
+      </DialogHeader>
+      <DialogFooter>
+        <Button type="submit">Confirm</Button>
+      </DialogFooter>
+    </DialogContent>
+  );
+
   return (
     <SidebarMenu>
       <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-              disabled={isLoading}
-            >
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">
-                  {!!activeTeam && activeTeam.name}
-                  {!!isLoading && "Loading"}
-                </span>
-              </div>
-              <ChevronsUpDown className="ml-auto" />
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-            align="start"
-            side={isMobile ? "bottom" : "right"}
-            sideOffset={4}
-          >
-            <DropdownMenuLabel className="text-muted-foreground text-xs">
-              Teams
-            </DropdownMenuLabel>
-            {teams.map((team) => (
-              <DropdownMenuItem
-                key={team.name}
-                onClick={() => setActiveTeam(team.id)}
-                className="gap-2 p-2"
-              >
-                {team.name}
-              </DropdownMenuItem>
-            ))}
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="gap-2 p-2">
-              <div className="bg-background flex size-6 items-center justify-center rounded-md border">
-                <Plus className="size-4" />
-              </div>
-              <div className="text-muted-foreground font-medium">Add team</div>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <Dialog
+          open={isCreateTeamDialogOpen}
+          onOpenChange={setIsCreateTeamDialogOpen}
+        >
+          {dropdown}
+          {dialogContent}
+        </Dialog>
       </SidebarMenuItem>
     </SidebarMenu>
   );
